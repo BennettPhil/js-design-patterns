e0d6a2201056dc88e10d37754eb3d7636a1d0bf7
Phil Bennett <phil@bleech.de>
2017-02-06 22:33:01 +0100

    fix(AbstractClass): Changing Error Types to Type Error

--- AbstractClass/Classes/AbstractClass.js
+++ AbstractClass/Classes/AbstractClass.js
@@ -1,22 +1,20 @@
 module.exports = class AbstractClass {
   constructor() {
     this.something = 'Something';
-    this.errorMessage = 'Abstract class "AbstractClass" cannot be instantiated directly';
+    this.errorMessage = 'Abstract class AbstractClass  cannot be instantiated directly';
     if (this.constructor === AbstractClass) {
       throw new TypeError(this.errorMessage);
     }
   }
 
   returnText() {
-    // TODO: Decided if this is the correct error type.
     this.errorMessage = 'Abtract Method returnText() cannot be called';
-    throw new Error(this.errorMessage);
+    throw new TypeError(this.errorMessage);
   }
 
   returnSomethingElse() {
-    // TODO: Decided if this is the correct error type. Do I need to create a new Error type?
     this.errorMessage = 'Abtract Method returnText() cannot be called';
-    throw new Error(this.errorMessage);
+    throw new TypeError(this.errorMessage);
   }
 
 };
