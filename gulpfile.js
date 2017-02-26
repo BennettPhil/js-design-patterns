a8771706f2983a6f3b7c245d3f5136eaabba1fda
Phil Bennett <phil@bleech.de>
2017-02-26 11:40:52 +0100

    build(Gulp Update): Updating gulp file and npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const babel = require('gulp-babel');
 
-gulp.task('lint', () => gulp.src(['**/*.js', '!node_modules/**']).pipe(eslint()).pipe(eslint.format()).pipe(eslint.failAfterError()));
+gulp.task('lint', () => {
+  gulp.src(['**/*.js', '!node_modules/**', '!dist/**']).pipe(eslint()).pipe(eslint.format()).pipe(eslint.failAfterError());
+});
 
-gulp.task('default', ['lint'], () => {
+gulp.task('babel', () => gulp.src(['**/*.js', '!node_modules/**', '!dist/**', 'gulpfile.js'])
+  .pipe(babel({
+    presets: ['es2015'],
+  }))
+  .pipe(gulp.dest('dist')));
 
+gulp.task('default', ['lint', 'babel'], () => {
+  gulp.watch(['**/*.js', '!node_modules/**', '!dist/**'], ['lint', 'babel']);
 });
